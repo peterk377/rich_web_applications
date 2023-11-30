513cb575d28b886bcaac18a23fefa4d68617eb64
peterk377 <peterk377@gmail.com>
2023-11-30 16:57:31 +0000

    added cart page

---
+++ src/app/api/getCart/route.js
@@ -0,0 +1,29 @@
+export async function GET(req, res) {
+
+    // Make a note we are on
+    // the api. This goes to the console.
+
+    console.log("in the api page")
+
+    // =================================================
+
+    const { MongoClient } = require('mongodb');
+
+    const url = 'mongodb+srv://peterk377:iB7xWTHkrG5tj4Sb@cluster0.bmv0a9n.mongodb.net/?retryWrites=true&w=majority';
+    const client = new MongoClient(url);
+
+    const dbName = 'app'; // database name
+
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('shopping_cart'); // collection name
+
+    const findResult = await collection.find({}).toArray();
+    console.log('Found documents =>', findResult);
+
+    //==========================================================
+    
+    // at the end of the process we need to send something back.
+    return Response.json(findResult)
+    }
---
+++ src/app/cart/page.js
@@ -0,0 +1,77 @@
+'use client';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import {ThemeProvider } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+import { useState, useEffect } from 'react'
+
+export default function Page() {
+const [data, setData] = useState(null)
+const [weather, setWeatherData] = useState(0)
+
+function putInCart(pname){
+    console.log("putting in cart: " + pname)
+    fetch("api/putInCart?pname="+pname);
+    }
+
+useEffect(() => {
+fetch('api/getCart')
+.then((res) => res.json())
+.then((data) => {
+setData(data)
+})
+
+fetch('api/getWeather')
+.then((res) => res.json())
+.then((weather) => {
+setWeatherData(weather)
+})
+
+
+}, [])
+if (!data) return <p>Loading</p>
+const theme = createTheme({
+palette: {
+secondary: {
+main: green[500],
+},
+},
+});
+
+if (!weather) return <p>No weather</p>
+
+return (
+<ThemeProvider theme={theme}>
+Today's temperature: {JSON.stringify(weather.temp)}
+<Container component="main" maxWidth="xs">
+<div style={{fontSize: '40px'}} > Dashboard</div>
+<div>
+{
+data.map((item, i) => (
+<div style={{padding: '20px'}} key={i} >
+Unique ID: {item._id}
+<br></br>
+{item.pname}
+-
+{item.price}
+<br></br>
+
+</div>
+))
+}
+</div>
+</Container>
+</ThemeProvider>
+);
+}
