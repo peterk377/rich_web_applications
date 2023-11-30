77489102d508ae2b8d3ba870ddd520bdf3f5da0f
peterk377 <peterk377@gmail.com>
2023-11-30 16:24:47 +0000

    new url strings added

--- src/app/api/getProducts/route.js
+++ src/app/api/getProducts/route.js
@@ -9,7 +9,7 @@ export async function GET(req, res) {
 
     const { MongoClient } = require('mongodb');
 
-    const url = 'mongodb+srv://peterk377:peterMongodb1@cluster0.bmv0a9n.mongodb.net/?retryWrites=true&w=majority';
+    const url = 'mongodb+srv://peterk377:iB7xWTHkrG5tj4Sb@cluster0.bmv0a9n.mongodb.net/?retryWrites=true&w=majority';
     const client = new MongoClient(url);
 
     const dbName = 'app'; // database name
